4db5db7e20e610117806a2586337999291d8079e
pamuba <pamuba@users.noreply.github.com>
2020-08-17 20:09:00 +0530

    2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React , {Component} from 'react';
 import './App.css';
+import Counter from './components/Counter';
 import Hello from './components/Hello';
+import Message from './components/Message';
 
 
 class App extends Component {
   render(){
     return (
       <div className="App">
-       <Hello name="Bruce"/>
-       <Hello name="Diana"/>
-       <Hello name="Clark"/>
+       {/* <Hello name="Bruce" heroName="Batman">
+         <p>This is from the first childern prop</p>
+         <p>This is from the second childern prop</p>
+       </Hello>
+       <Hello name="Diana" heroName="Wonder Women">
+         <button>CLICK</button>
+       </Hello>
+       <Hello name="Clark" heroName="Superman"/> */}
+
+       {/* <Message /> */}
+
+       <Counter></Counter>
       </div>
     );
   }
--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,9 +1,12 @@
 import React, { createElement } from 'react'
 
 const Hello = (props) => {
+    console.log({props})
+    // props.name = "new Name";
     return (
         <div>
-            <h1>Greetings {props.name}</h1>
+            <h1>Greetings {props.name} a.k.a {props.heroName}</h1>
+            {props.children}
         </div>
     )
 
